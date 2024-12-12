ed8d94710f630a8e85b21e9ddab3ecdb865f9705
QnX-Bits <tuananhh231@gmail.com>
2024-12-12 21:08:33 +0700

    Add files via upload

---
+++ Tháp Hà Nội/README.md
@@ -0,0 +1,7 @@
+# TowersWinformC-
+Towers of Hanoi C# winform
+
+Simple windows form towers of hanoi.
+
+This application is created to show how towers of Hanoi can be solved using recursion and each block is moved to visually display
+the moves required to solve this puzzle.
---
+++ Tháp Hà Nội/TowersWindows/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TowersWindows
+{
+    partial class Form1
+    {        
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        
+        private void InitializeComponent()
+        {
+            this.DiskCount = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.listMoves = new System.Windows.Forms.ListBox();
+            this.lblMoves = new System.Windows.Forms.Label();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.DiskCount)).BeginInit();
+            this.SuspendLayout();
+            
+            this.DiskCount.Location = new System.Drawing.Point(152, 15);
+            this.DiskCount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DiskCount.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.DiskCount.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.DiskCount.Name = "DiskCount";
+            this.DiskCount.Size = new System.Drawing.Size(69, 22);
+            this.DiskCount.TabIndex = 0;
+            this.DiskCount.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.DiskCount.ValueChanged += new System.EventHandler(this.DiskCount_ValueChanged);
+            
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập vào số đĩa ";
+            
+            this.btnSolve.Location = new System.Drawing.Point(247, 11);
+            this.btnSolve.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(133, 28);
+            this.btnSolve.TabIndex = 2;
+            this.btnSolve.Text = "Giải";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.BtnSolve_Click);
+            
+            this.listMoves.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listMoves.FormattingEnabled = true;
+            this.listMoves.ItemHeight = 16;
+            this.listMoves.Location = new System.Drawing.Point(1095, 47);
+            this.listMoves.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listMoves.Name = "listMoves";
+            this.listMoves.Size = new System.Drawing.Size(307, 484);
+            this.listMoves.TabIndex = 4;
+           
+            this.lblMoves.AutoSize = true;
+            this.lblMoves.Location = new System.Drawing.Point(1091, 15);
+            this.lblMoves.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMoves.Name = "lblMoves";
+            this.lblMoves.Size = new System.Drawing.Size(78, 16);
+            this.lblMoves.TabIndex = 5;
+            this.lblMoves.Text = "Các bước đi";
+        
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.Location = new System.Drawing.Point(388, 17);
+            this.lblCounter.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(174, 16);
+            this.lblCounter.TabIndex = 6;
+            this.lblCounter.Text = "Đường đi ngắn nhất {7} bước";
+            this.lblCounter.Click += new System.EventHandler(this.lblCounter_Click);
+
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(20, 47);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1067, 492);
+            this.panel1.TabIndex = 7;
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1419, 555);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.lblMoves);
+            this.Controls.Add(this.listMoves);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DiskCount);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Tháp Hà Nội";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DiskCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown DiskCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.ListBox listMoves;
+        private System.Windows.Forms.Label lblMoves;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
